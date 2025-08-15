a7556a1ca1f4da3f476142b066d6560d06d6e189
erogemy <erogemy.official@gmail.com>
2025-08-15 12:50:52 +0900

    tuning reflection

--- Assets/Scripts/View/Ball.cs
+++ Assets/Scripts/View/Ball.cs
@@ -11,12 +11,21 @@ namespace Erogemy.BlockBreaker.View
         Vector2 initPos;
         float reflectionPower;
 
+        bool isReflectionInFrame;
+        Vector2? lastBlockDiffInFrame; // 最後に接触したブロックとのdiff
+
         public UnityEvent OnBallFell { get; } = new();
         public UnityEvent OnRemoveBlock { get; } = new();
 
         void Start()
         {
             initPos = rectTransform.anchoredPosition;
+        }
+
+        void FixedUpdate()
+        {
+            isReflectionInFrame = false;
+            lastBlockDiffInFrame = null;
         }
 
         public void Init(float reflectionPower)
@@ -36,23 +45,16 @@ namespace Erogemy.BlockBreaker.View
             ballRb.linearVelocity = new Vector2(5f, 5f) * speed;
         }
 
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.TryGetComponent<Block>(out var block))
-            {
-                OnRemoveBlock.Invoke();
-                block.SetActive(false);
-            }
-        }
-
         void OnTriggerEnter2D(Collider2D collider)
         {
             if (collider.TryGetComponent<Paddle>(out var paddle))
             {
-                // パドルの中央に近いほど純粋な反射にする
-                // パドルの端に近いほど角度をつけて跳ね返す
+                if (isReflectionInFrame)
+                {
+                    return;
+                }
+
                 var normalPos = paddle.GetNormalPosition(rectTransform.anchoredPosition);
-                Debug.Log(normalPos);
 
                 var velocity = new Vector2(ballRb.linearVelocity.x, -ballRb.linearVelocity.y);
                 if (Mathf.Abs(normalPos) < 0.1f)
@@ -71,11 +73,108 @@ namespace Erogemy.BlockBreaker.View
                 newDirection = ApplyAngleLimit(newDirection, maxReflectionAngle);
 
                 ballRb.linearVelocity = newDirection * velocity.magnitude;
+                isReflectionInFrame = true;
             }
             else if (collider.TryGetComponent<FallArea>(out _))
             {
                 OnBallFell.Invoke();
             }
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.gameObject.TryGetComponent<Block>(out var block))
+            {
+                return;
+            }
+
+            OnRemoveBlock.Invoke();
+            block.SetActive(false);
+
+            if (isReflectionInFrame)
+            {
+                return;
+            }
+
+            var diff = rectTransform.anchoredPosition - block.GetPosition();
+            if (lastBlockDiffInFrame != null)
+            {
+                if (lastBlockDiffInFrame.Value.magnitude < diff.magnitude)
+                {
+                    // 前回のブロックとの接触の方が近ければ無視
+                    return;
+                }
+            }
+            lastBlockDiffInFrame = diff;
+
+            // ボールの速度をブロックと相対で取る(ブロック目線なので*-1しとく)
+            var relativeVelocity = -other.relativeVelocity;
+            var surface = CalcCollisionSurface(
+                rectTransform.anchoredPosition,
+                block.GetPosition(),
+                block.GetSize(),
+                relativeVelocity
+            );
+
+            if (surface.x != 0 && surface.y != 0)
+            {
+                // 角に衝突した時はどの角かで反射を制御
+                switch (surface.x)
+                {
+                    case < 0 when surface.y < 0:
+                        // 左下
+                        ballRb.linearVelocity　= new Vector2(
+                            - Mathf.Abs(relativeVelocity.x),
+                            - Mathf.Abs(relativeVelocity.y));
+                        break;
+                    case < 0 when surface.y > 0:
+                        // 左上
+                        ballRb.linearVelocity　= new Vector2(
+                            - Mathf.Abs(relativeVelocity.x),
+                            Mathf.Abs(relativeVelocity.y));
+                        break;
+                    case > 0 when surface.y < 0:
+                        // 右下
+                        ballRb.linearVelocity　= new Vector2(
+                            Mathf.Abs(relativeVelocity.x),
+                            - Mathf.Abs(relativeVelocity.y));
+                        break;
+                    default:
+                        // 右上
+                        ballRb.linearVelocity　= new Vector2(
+                            Mathf.Abs(relativeVelocity.x),
+                            Mathf.Abs(relativeVelocity.y));
+                        break;
+                }
+            }
+            else
+            {
+                ballRb.linearVelocity = relativeVelocity * new Vector2(
+                    surface.x != 0 ? -1 :1,
+                    surface.y != 0 ? -1 :1);
+            }
+        }
+
+        static Vector2 CalcCollisionSurface(Vector2 ballPosition, Vector2 blockPosition, Vector2 blockSize, Vector2 relativeVec)
+        {
+            // 対角線とdiffベクトルを比較してどの領域にいるかを判断
+            var diff = ballPosition - blockPosition;
+
+            // relativeVecの方向から考えられる衝突面を絞る
+            if (Mathf.Abs(blockSize.x) * 0.5f > Mathf.Abs(diff.x))
+            {
+                return new Vector2(0, relativeVec.y > 1 ? -1 : 1); // 上か下
+            }
+            else if (Mathf.Abs(blockSize.y) * 0.5f > Mathf.Abs(diff.y))
+            {
+                return new Vector2(relativeVec.x > 1 ? -1 : 1, 0); // 左か右
+            }
+
+            // 角の4つ
+            return new Vector2(
+                diff.x > 0 ? 1 : -1,
+                diff.y > 0 ? 1 : -1
+            );
         }
 
         Vector2 ApplyAngleLimit(Vector2 direction, float maxLimitAngle)
--- Assets/Scripts/View/Block.cs
+++ Assets/Scripts/View/Block.cs
@@ -20,13 +20,32 @@ namespace Erogemy.BlockBreaker.View
             rectTransform.anchoredPosition = pos;
 
             boxCollider.size = size;
-            // pivotが左下であることを前提にoffset調整
-            boxCollider.offset = size / 2;
+
+            // 中央に変更(テンプレprefabは左下pivot)
+            var globalPos = transform.position;
+            rectTransform.pivot = new Vector2(0.5f, 0.5f);
+            rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            transform.position = globalPos;
         }
 
         public void SetActive(bool isActive)
         {
             gameObject.SetActive(isActive);
+        }
+
+        public Vector2 GetPosition()
+        {
+            // 親のスケールを考慮
+            var globalScale = image.rectTransform.lossyScale;
+            return image.rectTransform.anchoredPosition * globalScale;
+        }
+
+        public Vector2 GetSize()
+        {
+            // 親のスケールを考慮
+            var globalScale = image.rectTransform.lossyScale;
+            return boxCollider.size * globalScale;
         }
     }
 }
